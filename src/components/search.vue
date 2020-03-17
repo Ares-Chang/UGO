5610f60b0eb17125dd48fb4bf1cbb8b0bffafc8b
Ares-Chang <2622310479@qq.com>
2020-03-17 19:47:54 +0800

    build: 页面构建导入更新

--- src/components/search.vue
+++ src/components/search.vue
@@ -1,134 +1,241 @@
 <template>
-  <!-- 搜索区 -->
-  <view class="search" :class="{'focus':focus}">
-		<view class="search_box">
-			<!-- 搜索框  -->
-			<input type="text" @focus="input_focus" :placeholder="placeholder" />
-			<text class="cancel" @click="this.focus=false;this.placeholder=''">取消</text>
-			<!-- 默认图标 -->
-			<view class="icon_1">搜索</view>
-			<!-- 聚焦图标 -->
-			<view class="icon_2"></view>
-		</view>
-		<!-- 历史记录 -->
-		<view class="res"></view>
-		<!-- 搜索列表 -->
-  </view>
+  <!-- 搜索 -->
+  <div class="search" :class="{focused: focused}">
+    <!-- 搜索框 -->
+    <div class="input-wrap" @click="goSearch">
+      <input type="text" :placeholder="placeholder">
+      <span class="cancle" @click.stop="cancleSearch">取消</span>
+    </div>
+    <!-- 搜索结果 -->
+    <div class="content">
+      <div class="title">搜索历史<span class="clear"></span></div>
+      <div class="history">
+        <navigator url="/pages/list/index">小米</navigator>
+        <navigator url="/pages/list/index">智能电视</navigator>
+        <navigator url="/pages/list/index">小米空气净化器</navigator>
+        <navigator url="/pages/list/index">西门子洗碗机</navigator>
+        <navigator url="/pages/list/index">华为手机</navigator>
+        <navigator url="/pages/list/index">苹果</navigator>
+        <navigator url="/pages/list/index">锤子</navigator>
+      </div>
+      <!-- 结果 -->
+      <scroll-view scroll-y class="result">
+        <navigator url="/pages/goods/index">小米</navigator>
+        <navigator url="/pages/goods/index">小米</navigator>
+        <navigator url="/pages/goods/index">小米</navigator>
+        <navigator url="/pages/goods/index">小米</navigator>
+        <navigator url="/pages/goods/index">小米</navigator>
+        <navigator url="/pages/goods/index">小米</navigator>
+        <navigator url="/pages/goods/index">小米</navigator>
+        <navigator url="/pages/goods/index">小米</navigator>
+        <navigator url="/pages/goods/index">小米</navigator>
+        <navigator url="/pages/goods/index">小米</navigator>
+        <navigator url="/pages/goods/index">小米</navigator>
+        <navigator url="/pages/goods/index">小米</navigator>
+        <navigator url="/pages/goods/index">小米</navigator>
+        <navigator url="/pages/goods/index">小米</navigator>
+        <navigator url="/pages/goods/index">小米</navigator>
+        <navigator url="/pages/goods/index">小米</navigator>
+        <navigator url="/pages/goods/index">小米</navigator>
+        <navigator url="/pages/goods/index">小米</navigator>
+      </scroll-view>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-	data() {
-		return {
-			focus: false,
-			placeholder: ''
-		}
-	},
-	methods: {
-		input_focus() {
-			this.focus = true
-			this.placeholder = '请输入你想要的商品'
+  export default {
+    data () {
+      return {
+        focused: false,
+        placeholder: ''
+      }
+    },
+    methods: {
+      goSearch (ev) {
+        this.focused = true;
+        this.placeholder = '请输入您要搜索的内容';
+        
+        // 触发父组件自定义事件
+        this.$emit('search', {
+          pageHeight: uni.getSystemInfoSync().windowHeight
+        });
 
-			// 获取窗口高度
-			var res = uni.getSystemInfoSync()
-			// 传递给父级
-			this.$emit('windowHeight',res.windowHeight)
-		}
-	}
-};
+        // 隐藏tabBar
+        uni.hideTabBar();
+      },
+      cancleSearch () {
+        this.focused = false;
+        this.placeholder = '';
+
+        // 触发父组件自定义事件
+        this.$emit('search', {
+          pageHeight: 'auto'
+        });
+
+        // 显示tabBar
+        uni.showTabBar();
+      }
+    }
+  }
 </script>
 
 <style lang="less" scoped>
-/* 搜索区 */
-// 选中状态
-.search {
-  background-color: #ff2d4a;
-  box-sizing: border-box;
-	padding: 20rpx 16rpx;
-	.search_box {
-		position: relative;
-		input {
-			width: 100%;
-			height: 60rpx;
-			background-color: #fff;
-			border-radius: 5rpx;
-		}
-		.cancel {
-			display: none;
-		}
-		.icon_1 {
-			display: block;
-			height: 32rpx;
-			line-height: 32rpx;
-			font-size: 24rpx;
-			position: absolute;
-			top: 50%;
-			left: 50%;
-			transform: translate(-50%,-50%);
-			background: url(http://static.botue.com/ugo/images/icon_search%402x.png) no-repeat;
-			background-size: 32rpx;
-			padding-left: 48rpx;
-			color: #bcbcbc;
-		}
-		.icon_2 {
-			display: none;
-		}
-	}
-	.res {
-		display: none;
-	}
-}
+  .search {
+    display: flex;
+    flex-direction: column;
+    
+    // 搜索框
+    .input-wrap {
+      display: flex;
+      height: 100rpx;
+      padding: 20rpx 30rpx;
+      background-color: #ea4451;
+      box-sizing: border-box;
+      position: relative;
 
-// 聚焦状态
-.focus {
-	position: absolute;
-	width: 100%;
-	height: 100%;
-	z-index: 999;
-	background-color: #eeeeee;
-	.search_box {
-		display: flex;
-		input {
-			flex: 1;
-			margin-right: 20rpx;
-			padding-left: 65rpx;
-			font-size: 28rpx;
-		}
-		.cancel {
-			width: 160rpx;
-			height: 60rpx;
-			line-height: 60rpx;
-			text-align: center;
-			border: 1px solid #cccccc;
-			font-size: 30rpx;
-			display: block;
-			box-sizing: border-box;
-			border-radius: 8rpx;
-		}
-		.icon_1 {
-			display: none;
-		}
-		.icon_2 {
-			display: block;
-			height: 32rpx;
-			width: 32rpx;
-			position: absolute;
-			top: 50%;
-			transform: translateY(-50%);
-			left: 20rpx;
-			background: url(http://static.botue.com/ugo/images/icon_search%402x.png) no-repeat;
-			background-size: 32rpx;
-			padding-left: 48rpx;
-		}
-	}
-	.res {
-		display: block;
-		position: absolute;
-		top: 92rpx;
-		bottom: 0;
-		left: 0;
-		right: 0;
-		background-color: #222;
-	}
-}
+      &::before,
+      &::after {
+        height: 44rpx;
+        line-height: 1;
+        background-image: url(http://static.botue.com/ugo/images/icon_search%402x.png);
+        background-size: 32rpx;
+        background-position: 6rpx center;
+        background-repeat: no-repeat;
+
+        position: absolute;
+        top: 28rpx;
+        z-index: 9;
+      }
+
+      &::before {
+        content: '搜索';
+        display: block;
+
+        width: 100rpx;
+        padding: 11rpx 0 10rpx 44rpx;
+        box-sizing: border-box;
+        color: #666;
+        font-size: 24rpx;
+        left: 325rpx;
+      }
+
+      &::after {
+        display: none;
+        content: '';
+        width: 44rpx;
+        left: 40rpx;
+      }
+
+      input {
+        flex: 1;
+        height: 60rpx;
+        padding: 0 20rpx 0 64rpx;
+        background-color: #fff;
+        border-radius: 8rpx;
+        font-size: 24rpx;
+        color: #666;
+      }
+
+      span.cancle {
+        display: none;
+        width: 80rpx;
+        text-align: right;
+        line-height: 60rpx;
+        font-size: 27rpx;
+        color: #666;
+      }
+    }
+
+    // 搜索结果
+    .content {
+      display: none;
+      flex: 1;
+      padding: 27rpx;
+      background-color: #fff;
+      position: relative;
+
+      .title {
+        font-size: 27rpx;
+        line-height: 1;
+        color: #333;
+      }
+
+      .clear {
+        display: block;
+        width: 27rpx;
+        height: 27rpx;
+        float: right;
+        background-image: url(http://static.botue.com/ugo/images/clear.png);
+        background-size: cover;
+      }
+
+      .history {
+        padding-top: 30rpx;
+
+        navigator {
+          display: inline-block;
+          line-height: 1;
+          padding: 15rpx 20rpx 12rpx;
+          background-color: #ddd;
+          font-size: 24rpx;
+          margin-right: 20rpx;
+          margin-bottom: 15rpx;
+          color: #333;
+        }
+      }
+
+      .result {
+        // display: none;
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        background-color: #fff;
+
+        navigator {
+          line-height: 1;
+          padding: 20rpx 30rpx;
+          font-size: 24rpx;
+          color: #666;
+          border-bottom: 1px solid #eee;
+
+          &:last-child {
+            border-bottom: none;
+          }
+        }
+      }
+    }
+    
+    // 获得焦点状态
+    &.focused {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      left: 0;
+      top: 0;
+      z-index: 9;
+
+      .input-wrap {
+        background-color: #eee;
+
+        &::before {
+          display: none;
+        }
+
+        &::after {
+          display: block;
+        }
+      }
+
+      span.cancle {
+        display: block;
+      }
+
+      .content {
+        display: block;
+      }
+    }
+  }
 </style>
