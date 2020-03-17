1141d8bcb5fb0d8d1abd3703a9291a761d65d8cf
Ares-Chang <2622310479@qq.com>
2020-03-17 21:36:49 +0800

    feat: 优化请求 loading

--- src/utils/request.js
+++ src/utils/request.js
@@ -6,9 +6,17 @@ export default function(obj) {
 		vue.prototype.request = async function(opts) {
 			const { url } = opts
 
+			// 添加 loading
+			uni.showLoading({
+				title: '数据加载中...'
+			})
+			// 设置请求
 			const [err,res] = await uni.request({
 				url: baseUrl + url
 			})
+
+			// 关闭loading
+			uni.hideLoading()
 
 			return res.data
 		}
